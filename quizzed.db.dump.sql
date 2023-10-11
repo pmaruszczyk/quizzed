c8ea284c7e261d8b66520c562a08e38ce61ad496
Pawel Maruszczyk <pmaruszczyk@future-processing.com>
2023-10-11 09:37:09 +0200

    db dump

---
+++ quizzed.db.dump.sql
@@ -0,0 +1,46 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.24-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.5.0.6677
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping data for table kotekhoot.answers: ~0 rows (approximately)
+
+-- Dumping data for table kotekhoot.failed_jobs: ~0 rows (approximately)
+
+-- Dumping data for table kotekhoot.migrations: ~4 rows (approximately)
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+	(1, '2014_10_12_000000_create_users_table', 1),
+	(2, '2014_10_12_100000_create_password_resets_table', 1),
+	(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+	(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+
+-- Dumping data for table kotekhoot.nick: ~0 rows (approximately)
+
+-- Dumping data for table kotekhoot.password_resets: ~0 rows (approximately)
+
+-- Dumping data for table kotekhoot.personal_access_tokens: ~0 rows (approximately)
+
+-- Dumping data for table kotekhoot.state: ~3 rows (approximately)
+INSERT INTO `state` (`id`, `value`) VALUES
+	('SHOWANSWER', 0),
+	('STEP', 0),
+	('STEPSTARTTIME', 1695915362);
+
+-- Dumping data for table kotekhoot.users: ~0 rows (approximately)
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
